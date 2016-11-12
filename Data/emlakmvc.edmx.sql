9095468681f01525552c52b37db7ed4ad39a958e
huolmez <olmezhuseyin7@gmail.com>
2016-11-13 02:31:31 +0300

    avatar tamamlandı

--- Data/emlakmvc.edmx.sql
+++ Data/emlakmvc.edmx.sql
@@ -2,7 +2,7 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 11/12/2016 21:49:40
+-- Date Created: 11/13/2016 00:59:44
 -- Generated from EDMX file: C:\Users\husey\Desktop\EMLAK MVC\Emlak\Data\emlakmvc.edmx
 -- --------------------------------------------------
 
