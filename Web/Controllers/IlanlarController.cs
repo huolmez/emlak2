02751442de5964e3596a752afd1465f4093a3527
huolmez <olmezhuseyin7@gmail.com>
2016-11-25 14:55:50 +0300

    Web kısmında ilanlar gösterildi

--- Web/Controllers/IlanlarController.cs
+++ Web/Controllers/IlanlarController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Data;
+
 
 namespace Web.Controllers
 {
